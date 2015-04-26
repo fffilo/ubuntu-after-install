5cb7359c872f3448d71880c0cb9e6cde60ef091f
fffilo <fffilo666@gmail.com>
2015-04-26 20:21:49 +0200

    Init script added

---
+++ src/020-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "################################"
+echo "### Aliases ####################"
+echo "################################"
+echo "" >> ~/.bashrc
+echo "# https://github.com/fffilo/ubuntu-after-install/blob/master/src/020-init.sh" >> ~/.bashrc
+echo "alias la='ls -lah'" >> ~/.bashrc
+echo "alias cdd='cd -'" >> ~/.bashrc
+echo "alias ..='cd ..'" >> ~/.bashrc
+echo "alias ...='cd ../..'" >> ~/.bashrc
+echo "alias ....='cd ../../..'" >> ~/.bashrc
+echo "alias .....='cd ../../../..'" >> ~/.bashrc
+echo "alias ......='cd ../../../../..'" >> ~/.bashrc
