22718a4469b5cf244166ca6b8be3dcc936ee5d15
fffilo <fffilo666@gmail.com>
2015-04-26 23:22:16 +0200

    Git config

--- src/260-git.sh
+++ src/260-git.sh
@@ -5,6 +5,8 @@ echo "################################"
 echo "### Git ########################"
 echo "################################"
 sudo apt-get install -y git git-core 
+# git config --global user.name "John Doe"
+# git config --global user.email johndoe@example.com
 git config --global alias.stat "status"
 git config --global alias.hist "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 chmod +x gitsound/gitsound.sh
