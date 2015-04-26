e34133b3ac11e5241d4527d56d090722cb7b0416
fffilo <fffilo666@gmail.com>
2015-04-26 20:48:46 +0200

    Git moved to init.sh

--- src/010-base.sh
+++ src/010-base.sh
@@ -5,6 +5,4 @@ echo "################################"
 echo "### Base install ###############"
 echo "################################"
 #sudo apt-get update
-sudo apt-get install -y build-essential software-properties-common python-software-properties python-dev python-pip git git-core openjdk-7-jdk curl
-git config --global alias.stat "status"
-git config --global alias.hist "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+sudo apt-get install -y build-essential software-properties-common python-software-properties python-dev python-pip openjdk-7-jdk curl
--- src/020-init.sh
+++ src/020-init.sh
@@ -2,6 +2,14 @@
 
 clear
 echo "################################"
+echo "### Git ########################"
+echo "################################"
+sudo apt-get install -y git git-core 
+git config --global alias.stat "status"
+git config --global alias.hist "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+clear
+echo "################################"
 echo "### Aliases ####################"
 echo "################################"
 echo "" >> ~/.bashrc
