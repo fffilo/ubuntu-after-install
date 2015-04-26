ba1afdb7d8590399f7a0d362a8a7c8aaabc805f2
fffilo <fffilo666@gmail.com>
2015-04-26 20:51:31 +0200

    Mounted storage linket to home directory

--- src/030-mount.sh
+++ src/030-mount.sh
@@ -10,4 +10,5 @@ if [[ $? -eq 1 ]]; then
 	echo "UUID=4372786d-6ae6-4fc8-98d3-d166655f1521    /storage    ext4    defaults    0    2" | sudo tee -a /etc/fstab
 	sudo mkdir -p /storage
 	sudo mount -a
+	ln -s /storage ~/storage
 fi
