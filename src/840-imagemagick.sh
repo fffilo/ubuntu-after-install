df16fb4b15dbca2b78736b196d8a2296e05bb207
fffilo <fffilo666@gmail.com>
2015-04-27 10:07:46 +0200

    ImageMagick PHP check

--- src/840-imagemagick.sh
+++ src/840-imagemagick.sh
@@ -3,4 +3,8 @@
 echo "################################"
 echo "### ImageMagick ################"
 echo "################################"
-sudo apt-get install -y imagemagick php5-imagick
+sudo apt-get install -y imagemagick
+php -v > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+	sudo apt-get install -y php5-imagick
+fi
