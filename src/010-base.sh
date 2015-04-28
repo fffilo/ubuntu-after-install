fe1834a797c5abffa701492df022aef771ad0076
fffilo <fffilo666@gmail.com>
2015-04-28 09:47:26 +0200

    linux-kernel-headers added to base.sh

--- src/010-base.sh
+++ src/010-base.sh
@@ -5,4 +5,4 @@ echo "################################"
 echo "### Base install ###############"
 echo "################################"
 #sudo apt-get update
-sudo apt-get install -y build-essential software-properties-common python-software-properties python-dev python-pip openjdk-7-jdk curl
+sudo apt-get install -y build-essential linux-kernel-headers software-properties-common python-software-properties python-dev python-pip openjdk-7-jdk curl
