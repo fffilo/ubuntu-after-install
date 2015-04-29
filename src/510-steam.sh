2cea8f3d69ddfb3cc8c8e028e256e51596209b48
fffilo <fffilo666@gmail.com>
2015-04-29 09:53:33 +0200

    steam-login

--- src/510-steam.sh
+++ src/510-steam.sh
@@ -6,4 +6,8 @@ echo "################################"
 #sudo debconf-set-selections <<< "steam steam/purge note"
 #sudo debconf-set-selections <<< "steam steam/question select I\ AGREE"
 #sudo debconf-set-selections <<< "steam steam/licence note"
-sudo apt-get install -y steam
+#sudo apt-get install -y steam
+
+sudo add-apt-repository -y ppa:thor27-gmail/steam-desktop 
+sudo apt-get update 
+sudo apt-get install -y steam steam-login
