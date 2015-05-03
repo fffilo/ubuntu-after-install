6da85655d64179257e8d092f45833b8f4225bd3f
fffilo <fffilo666@gmail.com>
2015-05-03 19:21:08 +0200

    GitSound fix

--- src/100-git.sh
+++ src/100-git.sh
@@ -9,6 +9,8 @@ sudo apt-get install -y git git-core
 # git config --global user.email johndoe@example.com
 git config --global alias.stat "status"
 git config --global alias.hist "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-chmod +x gitsound/gitsound.sh
-yes | ./gitsound/gitsound.sh
-rm gitsound -rf
+git clone https://github.com/fffilo/gitsound.git /tmp/gitsound
+chmod +x /tmp/gitsound/gitsound.sh
+yes | /tmp/gitsound/gitsound.sh
+rm /tmp/gitsound -rf
+
