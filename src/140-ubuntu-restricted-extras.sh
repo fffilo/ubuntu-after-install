53f994eb85766ea9846150899f2aec50dacf2ddf
fffilo <fffilo666@gmail.com>
2015-05-04 10:35:00 +0200

    ubuntu-restricted-extras.sh automatically accept the agreement

--- src/140-ubuntu-restricted-extras.sh
+++ src/140-ubuntu-restricted-extras.sh
@@ -5,5 +5,5 @@ echo ""
 echo "################################"
 echo "### Ubuntu restricted extras ###"
 echo "################################"
-#sudo debconf-set-selections <<< "???? ????/???? ????"
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt-get install -y ubuntu-restricted-extras
