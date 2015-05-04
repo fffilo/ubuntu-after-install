7d2f01be4a680b16ebad0a6512dfbcbed54ea721
fffilo <fffilo666@gmail.com>
2015-05-04 13:39:27 +0200

    steam.sh automatically accept the agreement

--- src/510-steam.sh
+++ src/510-steam.sh
@@ -8,8 +8,10 @@ echo "################################"
 #sudo debconf-set-selections <<< "steam steam/purge note"
 #sudo debconf-set-selections <<< "steam steam/question select I\ AGREE"
 #sudo debconf-set-selections <<< "steam steam/licence note"
-#sudo apt-get install -y steam
-
-sudo add-apt-repository -y ppa:thor27-gmail/steam-desktop
-sudo apt-get update
-sudo apt-get install -y steam steam-login
+#sudo add-apt-repository -y ppa:thor27-gmail/steam-desktop
+#sudo apt-get update
+#sudo apt-get install -y steam steam-login
+wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+sudo apt-get install -y libgl1-mesa-glx-lts-utopic:i386
+sudo dpkg -i steam.deb
+sudo rm steam.deb
