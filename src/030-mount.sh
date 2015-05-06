67786f76a75887ba0250ed107ffd2e0168c06da8
fffilo <fffilo666@gmail.com>
2015-05-06 11:10:36 +0200

    mount.sh uuid changed

--- src/030-mount.sh
+++ src/030-mount.sh
@@ -8,7 +8,7 @@ echo "################################"
 cat /etc/fstab | grep "4372786d-6ae6-4fc8-98d3-d166655f1521"
 if [[ $? -eq 1 ]]; then
 	echo "# storage" | sudo tee -a /etc/fstab >/dev/null
-	echo "UUID=4372786d-6ae6-4fc8-98d3-d166655f1521    /storage    ext4    defaults    0    2" | sudo tee -a /etc/fstab
+	echo "UUID=a99ff6b1-e25d-4ea5-9205-77aa880d7c40    /storage    ext4    defaults    0    2" | sudo tee -a /etc/fstab
 	sudo mkdir -p /storage
 	sudo mount -a
 	ln -s /storage ~/storage
